0d5ee666735572e627aac41527be16515fb1c987
Luan <jadsonluan1@gmail.com>
2018-11-27 16:50:15 -0300

    Implementa entidade abstrata

--- backend/src/main/java/br/edu/ufcg/ccc/pharma/model/AbstractEntity.java
+++ backend/src/main/java/br/edu/ufcg/ccc/pharma/model/AbstractEntity.java
@@ -1,4 +1,34 @@
 package br.edu.ufcg.ccc.pharma.model;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
 
+@MappedSuperclass
 public class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
